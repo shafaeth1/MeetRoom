d0059b9aa67cebcc256681c60523fb62ace18f0e
hasan-mia <hasanrafi69@gmail.com>
2022-07-26 05:07:18 +0600

    added header tags

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div class="navbar ">
-            <div class="flex-1">
-                <a href='#' class="btn btn-ghost normal-case text-xl">MeetRoom</a>
+        <header>
+            <div class="navbar ">
+                <div class="flex-1">
+                    <Link to="/" class="btn btn-ghost normal-case text-xl">MeetRoom</Link>
+                </div>
+                <div class="flex-none">
+                    <ul class="menu menu-horizontal p-0 text-xl">
+                        <li><a href='join#'>Join</a></li>
+                        <li><a href='support#'>Support</a></li>
+                        <li><Link to="/signIn">Sign In</Link></li>
+                    </ul>
+                </div>
             </div>
-            <div class="flex-none">
-                <ul class="menu menu-horizontal p-0 text-xl">
-                    <li><a>Join</a></li>
-                    <li><a>Support</a></li>
-                    <li><Link to="/signIn">Sign In</Link></li>
-                </ul>
-            </div>
-        </div>
+        </header>
     );
 };
 
