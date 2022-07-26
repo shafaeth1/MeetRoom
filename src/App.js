f24ee9db81958b28889b990d6b98125d73ab59ef
hasan-mia <hasanrafi69@gmail.com>
2022-07-26 14:23:09 +0600

    added about link

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}> </Route>
         <Route path="/signIn" element={<SignIn />}> </Route>
+        <Route path="/about" element={<About />}> </Route>
       </Routes>
       <Footer/>
     </>
