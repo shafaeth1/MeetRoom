de28c7366ae1c734e0618401008db14c08a15094
hasan-mia <hasanrafi69@gmail.com>
2022-07-28 16:35:40 +0600

    fix css issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import About from './pages/About/About';
