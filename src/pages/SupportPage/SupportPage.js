1903c2ee5ee8ea474f7eaa37b1a114e8898c89f7
alamin <alamin.education1997@gmail.com>
2022-07-29 09:49:08 +0600

    support page

--- src/pages/SupportPage/SupportPage.js
+++ src/pages/SupportPage/SupportPage.js
@@ -5,7 +5,6 @@ const SupportPage = () => {
     return (
         <div className=" grid lg:grid-rows-1 items-center my-20" >
             <div className=" w-96 mx-auto  text-center">
-                {/* <HiAtSymbol/> */}
                 <span class="footer-title">How can we help you?</span>
                 <div class="relative form-control w-96 my-5 ">
                     <input type="text" placeholder="describe your issues" class="input input-bordered w-full pr-16" />
