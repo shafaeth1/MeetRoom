b98bdfeb89c57dfd5f5391fc5d9dce777d882e83
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-07-29 12:27:44 +0600

    sign out btn

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,15 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+    const [user] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+       
+      };
     return (
         <header>
             <div class="navbar border-b-2">
@@ -12,7 +20,8 @@ const Navbar = () => {
                     <ul class="menu menu-horizontal p-0 md:text-xl">
                         <li><Link to='/join'>Join</Link></li>
                         <li><a href='support#'>Support</a></li>
-                        <li><Link to="/signIn">Sign In</Link></li>
+                        <li>{user ? <button  onClick={logout}  className="">Sign Out</button>
+:<Link to="/login">Sign In</Link>}</li>
                     </ul>
                 </div>
             </div>
