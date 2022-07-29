0f007832ab19c11aa2aa9c564561c9b365e11b0a
alamin <alamin.education1997@gmail.com>
2022-07-29 12:44:53 +0600

    add time

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+
+    const showdate = new Date()
+    const time = showdate.getHours() + ":" + showdate.getMinutes();
+
     return (
-        <header>
+        <header className="px-4">
             <div class="navbar border-b-2">
                 <div class="flex-1">
                     <Link to="/" class="btn btn-ghost normal-case md:text-xl">MeetRoom</Link>
                 </div>
-                <div class="flex-none">
+                <div class="flex flex-column justify-content-center align-items-center">
+                    <span className="text-xl font-bold px-10">{time}</span>
                     <ul class="menu menu-horizontal p-0 md:text-xl">
                         <li><Link to='/join'>Join</Link></li>
                         <li><a href='support#'>Support</a></li>
                         <li><Link to="/signIn">Sign In</Link></li>
+
                     </ul>
                 </div>
             </div>
