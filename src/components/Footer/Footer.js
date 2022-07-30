96548e9f697f3de95f7ca0541ae9921810accbd6
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-07-31 02:58:07 +0600

    add Slide on video path

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,28 @@ const Footer = () => {
         <footer>
             <div class="footer flex flex-wrap justify-between py-10 bg-neutral text-neutral-content px-2 lg:px-12">
                 <div>
-                    <span class="footer-title">About</span> 
+                    <span class="footer-title">About</span>
                     <Link to="/about">About us</Link>
                     <Link to="/contact">Contact us</Link>
                     <a class="link link-hover">Jobs</a>
                     <a class="link link-hover">Press kit</a>
-                </div> 
+                </div>
                 <div>
-                    <span class="footer-title">Download</span> 
+                    <span class="footer-title">Download</span>
                     <a class="link link-hover">Branding</a>
                     <a class="link link-hover">Design</a>
                     <a class="link link-hover">Marketing</a>
                     <a class="link link-hover">Advertisement</a>
-                </div> 
+                </div>
                 <div>
-                    <span class="footer-title">Feature</span> 
+                    <span class="footer-title">Feature</span>
                     <Link to='/single' class="link link-hover">Single</Link>
                     <Link to='/receive' class="link link-hover">Receive</Link>
                     <Link to='/chat' class="link link-hover">Chat</Link>
-                    <a class="link link-hover">Video</a>
+                    <Link to='/video' class="link link-hover">Video</Link>
                 </div>
                 <div>
-                    <span class="footer-title">Legal</span> 
+                    <span class="footer-title">Legal</span>
                     <a class="link link-hover">Terms of use</a>
                     <a class="link link-hover">Privacy policy</a>
                     <a class="link link-hover">Cookie policy</a>
@@ -37,8 +37,8 @@ const Footer = () => {
                 <p className='text-sm'>All right reserved by MeetRoom &copy; 2022</p>
                 <div class="grid grid-flow-col gap-2">
                     <i className='fab fa-facebook text-md'></i>
-                    <i className='fab fa-twitter text-md'></i>  
-                    <i className='fab fa-instagram text-md'></i>                  
+                    <i className='fab fa-twitter text-md'></i>
+                    <i className='fab fa-instagram text-md'></i>
                 </div>
             </div>
         </footer>
--- src/pages/GroupCall/GroupCall.js
+++ src/pages/GroupCall/GroupCall.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Slider from '../../components/Slider/Slider';
 
 const GroupCall = () => {
     return (
-        <div className="flex mx-auto h-screen justify-content-center align-items-center z-[-100]">
-            <video className="video-container w-full" controls>
+        <div className="">
+            <div className="flex mx-auto h-screen justify-content-center align-items-center z-[-100]">
+                <video className="video-container w-full" controls>
 
-            </video>
+                </video>
+
+            </div>
+            <Slider></Slider>
         </div>
     );
 };
