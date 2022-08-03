939d794cfebf1aa8b3c499555be2b61d53b0da0c
hasan-mia <hasanrafi69@gmail.com>
2022-08-03 12:03:28 +0600

    fix hossain vai schedule responsive

--- src/pages/ConferenceRoom/ScheduleConference/ScheduleConference.js
+++ src/pages/ConferenceRoom/ScheduleConference/ScheduleConference.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MeetingSchedule from '../../../components/MeetingSchedule/MeetingSchedule'
 
 const ScheduleConference = () => {
     return (
         <div className="flex justify-center gap-1 flex-col lg:flex-row">
             <div className="md:w-12/12 lg:w-9/12">
-                <h1 className='text-center text-gray-200'>Schedule</h1>
+                <MeetingSchedule/>
             </div>
             {/* ========Right Sidebar ========*/}
             <div className="md:w-12/12 lg:w-3/12">
