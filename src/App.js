7b766745895404f3c67139d04f5c5af3761de014
HappyCoders <happycoders506@gmail.com>
2022-08-03 16:42:24 +0600

    schduls implement

--- src/App.js
+++ src/App.js
@@ -18,10 +18,7 @@ import LiveChat from './components/LiveChat/LiveChat';
 import RequireAuth from './pages/Register/RequireAuth';
 import auth from './firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
-<<<<<<< HEAD
-import Participant from './components/Participant/Participant';
-import Schdul from './components/Schdule/Schdul';
-=======
+
 import UserConference from './pages/ConferenceRoom/UserConference/UserConference';
 import ConferenceRoom from './pages/ConferenceRoom/ConferenceRoom';
 import VideoConference from './pages/ConferenceRoom/VideoConference/VideoConference';
@@ -30,7 +27,6 @@ import ScheduleConference from './pages/ConferenceRoom/ScheduleConference/Schedu
 import NotificationConference from './pages/ConferenceRoom/NotificationConference/NotificationConference';
 import SettingConference from './pages/ConferenceRoom/SettingConference/SettingConference';
 import MeetingSchedule from './components/MeetingSchedule/MeetingSchedule';
->>>>>>> a6039a76dde071a6126fbf0542446be5848d6101
 
 function App() {
   const [user] = useAuthState(auth);
@@ -47,7 +43,6 @@ function App() {
         <Route path="/ariful" element={<Ariful />}> </Route>
         <Route path="/nibras" element={<Nibras />}> </Route>
 
-        {/*============== Frontend Route ====================*/}
         <Route path="/" element={<Home />}> </Route>
         <Route path="/signIn" element={<SignIn />}> </Route>
         <Route path="/signup" element={<SignUp />}> </Route>
@@ -55,23 +50,17 @@ function App() {
         <Route path="/about" element={<About />}> </Route>
         <Route path="/contact" element={<Contact />}> </Route>
         <Route path="/liveChat" element={<LiveChat />}> </Route>
-        <Route path="/schedule" element={<MeetingSchedule />}> </Route>     
+        <Route path="/schedule" element={<MeetingSchedule />}> </Route>
 
-        {/* ============Video Conference Room Route ===============*/}
         <Route path="/room" element={<RequireAuth><ConferenceRoom /></RequireAuth>}>
-          <Route index element={<HomeConference/>}></Route>
-          <Route path="users" element={<UserConference/>}></Route>
-          <Route path="video" element={<VideoConference/>}></Route>
-          <Route path="schedule" element={<ScheduleConference/>}></Route>
-          <Route path="notifications" element={<NotificationConference/>}></Route>
-          <Route path="settings" element={<SettingConference/>}></Route>
+          <Route index element={<HomeConference />}></Route>
+          <Route path="users" element={<UserConference />}></Route>
+          <Route path="video" element={<VideoConference />}></Route>
+          <Route path="schedule" element={<ScheduleConference />}></Route>
+          <Route path="notifications" element={<NotificationConference />}></Route>
+          <Route path="settings" element={<SettingConference />}></Route>
         </Route>
-<<<<<<< HEAD
-        <Route path="/chat" element={<Chat />}> </Route>
-        <Route path="/Schdul" element={<Schdul />}> </Route>
-        <Route path="/participant" element={<Participant />}> </Route>
-=======
->>>>>>> a6039a76dde071a6126fbf0542446be5848d6101
+
       </Routes>
       {!user ? <Footer /> : ''}
     </>
