a57b7f8bbfb474c5eacee43dfa4d82a847a2bbeb
Mossahid123 <mossahidhossain309@gmail.comm>
2022-08-25 01:20:03 +0600

    changed on broadcast

--- client/src/ConferenceRoom/ModalConference/CreateBroadcastRoom.js
+++ client/src/ConferenceRoom/ModalConference/CreateBroadcastRoom.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const CreateBroadcastRoom = () => {
 
-    
+
     return (
         <div className=' items-center justify-center'>
             <label for="my-modal-4" className="hover:cursor-pointer"><i className="far fa-signal-stream font-bold text-3xl bg-clock bg-transparent border border-gray-300 p-2"></i></label>
@@ -12,7 +12,7 @@ const CreateBroadcastRoom = () => {
             
             <div className="modal modal-bottom sm:modal-middle  ">
                 <div className="modal-box bg-chat h-auto  mx-auto  ">
-                    <h2 className="text-center text-2xl divide-y">Crate Room BroadCast</h2>
+                    <h2 className="text-center text-2xl divide-y">Crate Room Broadcast</h2>
                     <div className='flex justify-end gap-3'>
                         {/* Cancel Btn */}
                         <div className="modal-action">
