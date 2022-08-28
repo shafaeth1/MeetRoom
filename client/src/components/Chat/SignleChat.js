938ffb50d7fd8a58e4befc706194807fc88f9fce
hasan-mia <hasanrafi69@gmail.com>
2022-08-29 02:37:57 +0600

    added chat on single video conference page

--- client/src/components/Chat/SignleChat.js
+++ client/src/components/Chat/SignleChat.js
@@ -1,58 +1,13 @@
 
 import React from 'react';
-import user from "../../assets/images/user.png"
 
 const SignleChat = ({text, handleChange, messages, renderMessage, sendMessage}) => {
     return (
         <div className='w-full border-x border-b border-green-700'>
-            <div className='rounded-lg mb-2 tab-bar overflow-y-auto max-h-96 h-96'>
-            <div className='flex items-center py-1 mb-1 justify-start gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 mr-1 rounded-full' />
-                    <div>
-                        <h2 className='text-md font-medium text-slate-200'>Hasan Rafi</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-
-                <div className='flex items-center py-1 mb-1 flex-row-reverse text-right pr-1 gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 ml-1 rounded-full' />
-                    <div >
-                        <h2 className='text-md font-medium text-slate-200'>You</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-                <div className='flex items-center py-1 mb-1 justify-start gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 mr-1 rounded-full' />
-                    <div>
-                        <h2 className='text-md font-medium text-slate-200'>Hasan Rafi</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-
-                <div className='flex items-center py-1 mb-1 flex-row-reverse text-right pr-1 gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 ml-1 rounded-full' />
-                    <div >
-                        <h2 className='text-md font-medium text-slate-200'>You</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-                <div className='flex items-center py-1 mb-1 justify-start gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 mr-1 rounded-full' />
-                    <div>
-                        <h2 className='text-md font-medium text-slate-200'>Hasan Rafi</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-
-                <div className='flex items-center py-1 mb-1 flex-row-reverse text-right pr-1 gap-y-1'>
-                    <img src={user} alt="Main user" className='w-12 h-12 p-1 border border-slate-600 ml-1 rounded-full' />
-                    <div >
-                        <h2 className='text-md font-medium text-slate-200'>You</h2>
-                        <p className='text-sm bg-slate-200 p-1 rounded'>Guys did i join late?</p>
-                    </div>
-                </div>
-                {messages?.map(renderMessage)}
-
+            <div className='rounded-lg mb-2 tab-bar overflow-y-auto max-h-96 h-96 px-2'>
+                
+                { messages?.map(renderMessage)}
+    
             </div>
 
 
