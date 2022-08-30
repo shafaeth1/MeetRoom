b7b7c8ded893b3df60424fa968c7a541af876f55
hasan-mia <hasanrafi69@gmail.com>
2022-08-30 19:57:21 +0600

    restyle video layout

--- client/src/components/Chat/SignleChat.js
+++ client/src/components/Chat/SignleChat.js
@@ -4,14 +4,14 @@ import React from 'react';
 const SignleChat = ({text, handleChange, messages, renderMessage, sendMessage}) => {
     return (
         <div className='w-full border-x border-b border-green-700'>
-            <div className='rounded-lg mb-2 tab-bar overflow-y-auto max-h-96 h-96 px-2'>
+            <div className='rounded-lg mb-2 tab-bar overflow-y-auto max-h-80 h-80 px-2'>
                 
                 { messages?.map(renderMessage)}
     
             </div>
 
 
-            <div className='flex items-center gap-2 bg-green-600 rounded p-2 mt-4'>
+            <div className='flex items-center gap-2 bg-green-600 rounded p-2 mt-6'>
                 <button><i className="far fa-image text-3xl text-gray-200"></i></button>
                 <input type="text" value={text} onChange={handleChange} placeholder="Type here" className="p-2 rounded-md border border-slate-600-none w-full px-2" />
                 <button onClick={sendMessage}> <i className="far fa-paper-plane text-2xl font-medium text-gray-200"></i> </button>
